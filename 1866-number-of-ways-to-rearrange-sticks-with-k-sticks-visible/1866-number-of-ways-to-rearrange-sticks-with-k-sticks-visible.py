d12e3d2dabacb47069cbed80450b037ade80908b
Vishva Mane <168759691+VishvaMane@users.noreply.github.com>
2026-06-07 15:02:48 +0530

    Time: 782 ms (83.43%), Space: 19.3 MB (97.55%) - LeetHub

---
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rearrangeSticks(self, n: int, k: int) -> int:
+        MOD = 10**9 + 7
+        
+        dp = [0] * (k + 1)
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            new_dp = [0] * (k + 1)
+            for j in range(1, min(i, k) + 1):
+                new_dp[j] = (dp[j-1] + (i-1) * dp[j]) % MOD
+            dp = new_dp
+        
+        return dp[k]
