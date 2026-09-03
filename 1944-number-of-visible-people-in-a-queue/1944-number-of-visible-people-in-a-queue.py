203e4b8735bfdaf9fb6bb51648a3bcd4dd5a79b0
Vishva Mane <168759691+VishvaMane@users.noreply.github.com>
2026-09-03 09:09:43 +0530

    Time: 67 ms (98.54%), Space: 31.7 MB (83.95%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canSeePersonsCount(self, heights: list[int]) -> list[int]:
+        n = len(heights)
+        ans = [0] * n
+        stack = []
+        
+        for i in range(n - 1, -1, -1):
+            count = 0
+            while stack and stack[-1] < heights[i]:
+                stack.pop()
+                count += 1
+                
+            if stack:
+                count += 1
+                
+            ans[i] = count
+            stack.append(heights[i])
+            
+        return ans
